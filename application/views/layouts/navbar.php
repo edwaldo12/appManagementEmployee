9a09faf5861f21d1254d17be252ffebd7034e5e1
Edwaldo Utama <53479749+edwaldo12@users.noreply.github.com>
2021-10-15 20:30:04 +0700

    make logout

--- application/views/layouts/navbar.php
+++ application/views/layouts/navbar.php
@@ -6,5 +6,17 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
     </ul>
+    <ul class="navbar-nav ml-auto">
+        <!-- Navbar Search -->
+        <?php
+        $login = $this->session->userdata('user');
+        if (empty($login)) { ?>
+
+        <?php } else { ?>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="<?php echo site_url('login/logout') ?>" class="nav-link">Logout</a>
+            </li>
+        <?php } ?>
+    </ul>
 </nav>
 <!-- /.navbar -->
